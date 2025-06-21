d40323c7ded4375d1442725a671078930deb144a
olger856 <infaccallo@gmail.com>
2025-06-21 17:22:06 -0500

    cambio11

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "upeu-terraform-state"
+    key    = "s3/main.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
